95c56a8b87dfcb55ac11add836532e805693bddb
vih <raianev77@gmail.com>
2025-07-30 20:37:50 -0300

    Adição de caracteres

--- calculadora/src/calculadora/Calculadora.java
+++ calculadora/src/calculadora/Calculadora.java
@@ -1,20 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package calculadora;
 
-/**
- *
- * @author raian
- */
+import calculadora.view.JFrTela;
+
 public class Calculadora {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new JFrTela().setVisible(true);
+            }
+        });
+        
     }
     
 }
